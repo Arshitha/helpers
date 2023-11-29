d5d14d3d5357cd1b5c0f5be59c8031aa45ffb5e0
Arshitha Basavaraj <arsh2794@proton.me>
2023-11-29 16:20:55 -0500

    Update python_template.py

--- python_script_boilerplate.py
+++ python_script_boilerplate.py
@@ -19,16 +19,9 @@ def get_args():
     return args.inputdir.resolve(), args.outdir.resolve()
 
 
-def run(cmdstr, logfile):
-    """Runs the given command str as shell subprocess. If logfile object is provided, then the stdout and stderr of the
-    subprocess is written to the log file.
-    :param str cmdstr: A shell command formatted as a string variable.
-    :param io.TextIOWrapper logfile: optional, File object to log the stdout and stderr of the subprocess.
-    """
-    if not logfile:
-        subprocess.run(cmdstr, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf8', shell=True)
-    else:
-        subprocess.run(cmdstr, stdout=logfile, stderr=subprocess.STDOUT, encoding='utf8', shell=True)
+def run(cmdstr):
+    p = subprocess.run(cmdstr, stdout=logfile, stderr=subprocess.STDOUT, encoding='utf8', shell=True)
+    return p.stdout
 
 
 def main():
