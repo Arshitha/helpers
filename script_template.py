e5d4d4aab52a35dc745aa7c2588dc7c9b0baf3d1
Arshitha Basavaraj <arsh2794@proton.me>
2023-11-29 22:35:54 -0500

    Update and rename python_script_boilerplate.py to script_template.py

--- script_template.py
+++ script_template.py
@@ -10,9 +10,9 @@ from pathlib import Path
 def get_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, description=__doc__)
 
-    parser.add_argument('-i', '--input', type=Path, action='store', dest='inputdir', metavar='INPUT_DIR',
+    parser.add_argument('-i', '--input', type=Path, action='store', dest='input_dir', metavar='INPUT_DIR',
                         help='Path to input BIDS directory.')
-    parser.add_argument('-o', '--output', type=Path, action='store', dest='outdir', metavar='OUTPUT_DIR',
+    parser.add_argument('-o', '--output', type=Path, action='store', dest='output_dir', metavar='OUTPUT_DIR',
                         default=Path('.'), help="Path to directory that'll contain the outputs.")
     args = parser.parse_args()
 
