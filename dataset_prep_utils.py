387caa9b3b694dedf22cc014562392da5a5e9141
Arshitha Basavaraj <arsh2794@proton.me>
2023-11-29 22:34:56 -0500

    Create dataset_prep_utils.py

---
+++ dataset_prep_utils.py
@@ -0,0 +1,52 @@
+import subprocess
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+
+def run(cmdstr):
+    p = subprocess.run(cmdstr, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf8', shell=True)
+    return p.stdout
+
+
+def set_participant_contributions(files_list, inventory_df, column_name):
+    prev_rowidx = inventory_df.shape[0]
+    for f in files_list:
+        curr_rowidx = prev_rowidx + 1
+        pid = f.parts[-3]
+        if not inventory_df.loc[inventory_df['participant_id'] == pid, 'participant_id'].values:
+            inventory_df.at[rowidx, 'participant_id'] = pid
+            inventory_df.at[rowidx, column_name] = 1
+            prev_rowidx = curr_rowidx
+        else:
+            inventory_df.loc[inventory_df['participant_id'] == pid, column_name] = 1
+
+
+def set_tab_data_contributions(phenotype_files):
+  for pf in phenotype_files:
+    df = pd.read_csv(pf, sep='\t', keep_default_na=False)
+    for idx, row in df.iterrows():
+        # remove rows with all n/a's except participant_id
+        uniq_elements = set(row[1:])
+        if len(uniq_elements) == 1 and list(uniq_elements)[0] == 'n/a':
+            pass
+        else:
+            pid = row['participant_id']
+            if not actual_ptsv_df.loc[actual_ptsv_df['participant_id'] == pid, 'participant_id'].values:
+                actual_ptsv_df.at[rowidx, 'participant_id'] = pid
+                actual_ptsv_df.at[rowidx, pf.name.split('.')[0]] = 1
+                rowidx += 1
+            else:
+                actual_ptsv_df.loc[actual_ptsv_df['participant_id'] == pid, pf.name.split('.')[0]] = 1
+  
+
+def replace_nans(df, replacement_str):
+    for col in df.columns:
+        for idx, val in df[col].items():
+            if val in ['NA', '', np.NaN]:
+                if not replacement_str:
+                    df.at[idx, col] = 'n/a'
+                else:
+                    df.at[idx, col] = int(replacement_str)
+    return df
